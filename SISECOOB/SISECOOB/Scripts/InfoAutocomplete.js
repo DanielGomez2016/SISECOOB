1646479cf0175ab6efd395b4f0050d0076a5b141
Fernando gomez <dgomezpayan@gmail.com>
2016-03-04 12:37:42 -0700

    librerias

---
+++ SISECOOB/SISECOOB/Scripts/InfoAutocomplete.js
@@ -0,0 +1,5 @@
+﻿// Template para cargar informacion a mostrar en los resultados de los autocompletes
+var InfoAutocomplete = {
+    promovido: '<div><b>{nombreCompleto}</b><br><b>Domicilio:</b> {calle} {numeroExterior} {colonia}<br><b>OCR:</b> {ocr} <b>Clave Elector:</b> {claveElector} <b>Folio Nacional:</b> {folioNacional}<br/> <b>RFC:</b> {rfc} <b>CURP:</b> {curp}<br><b>Edad:</b> {edad} <b> Sexo:</b> {sexo} <b>Promovido</b>: {estaPromovido}</div>',
+    promotor: '<div><b>{nombreCompleto}</b><br><b>Domicilio:</b> {calle} {numeroExterior} {colonia}<br><b>OCR:</b> {ocr} <b>Clave Elector:</b> {claveElector} <b>Folio Nacional:</b> {folioNacional}<br/> <b>RFC:</b> {rfc} <b>CURP:</b> {curp}<br><b>Edad:</b> {edad} <b> Sexo:</b> {sexo} <b>Promovido</b>: {estaPromovido}</div>'
+}
