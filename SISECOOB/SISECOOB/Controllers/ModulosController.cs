41f9cfb79f62a5d0850958b911964de0b9f9a595
Fernando gomez <dgomezpayan@gmail.com>
2016-03-23 11:12:25 -0700

    controlador de modulo

--- SISECOOB/SISECOOB/Controllers/ModulosController.cs
+++ SISECOOB/SISECOOB/Controllers/ModulosController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SISECOOB.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,123 @@ namespace SISECOOB.Controllers
         // GET: Modulos
         public ActionResult Index()
         {
+            SISECOOBEntities db = new SISECOOBEntities();
+            ViewBag.ModuloP = db.Menu.Where(i => i.Padre == 0).Select(i => new { id = i.MenuID, nombre = i.Nombre }).ToList();
             return View();
         }
+
+        public JsonResult Buscar(string nombre, int padre = 0, int page = 1, int pageSize = 15)
+        {
+            SISECOOBEntities db = new SISECOOBEntities();
+
+            IQueryable<Menu> query = db.Menu;
+
+            if (!string.IsNullOrEmpty(nombre))
+                query = query.Where(i => (i.Nombre).Contains(nombre));
+            if (padre > 0)
+                query = query.Where(i => i.Padre == padre);
+
+            return Json(new
+            {
+                total = query.Count(),
+                datos = query
+                     .OrderBy(i => new { i.MenuID, i.Padre })
+                     .Select(i => new {
+                         menuid = i.MenuID,
+                         nombre = i.Nombre,
+                         direccion = i.Controlador + "/" + i.Direccion,
+                         padre = i.Padre,
+                     }).ToList()
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Formulario(int id)
+        {
+
+            SISECOOBEntities db = new SISECOOBEntities();
+            Menu menu = db.Menu.FirstOrDefault(i => i.MenuID == id);
+
+            ViewBag.ModuloP = db.Menu.Where(i => i.Padre == 0).Select(i => new { id = i.MenuID, nombre = i.Nombre }).ToList();
+
+            Menu m = new Menu();
+
+            if (menu != null)
+            {
+                m.MenuID = menu.MenuID;
+                m.Nombre = menu.Nombre;
+                m.Direccion = menu.Direccion;
+                m.Controlador = menu.Controlador;
+                m.Padre = menu.Padre;
+                m.Nivel = menu.Nivel;
+            }
+
+            return PartialView("_Formulario", m);
+        }
+
+        [HttpPost]
+        public JsonResult Create(Menu menu)
+        {
+            try
+            {
+                menu.Crear();
+                return Json(new
+                {
+                    result = true
+                });
+            }
+            catch (Exception e)
+            {
+                return Json(new
+                {
+                    result = false,
+                    message = e.Message
+                });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Edicion(Menu menu)
+        {
+            try
+            {
+                menu.Editar();
+                return Json(new
+                {
+                    result = true
+                });
+            }
+            catch (Exception e)
+            {
+                return Json(new
+                {
+                    result = false,
+                    message = e.Message
+                });
+            }
+        }
+
+        [HttpPost]
+        public JsonResult Elimina(int id = 0)
+        {
+            try
+            {
+                Menu menu = new Menu();
+                menu.Eliminar(id);
+                return Json(new
+                {
+                    result = true
+                });
+            }
+            catch (Exception e)
+            {
+                return Json(new
+                {
+                    result = false,
+                    message = e.Message
+                });
+            }
+        }
     }
+}
 }
